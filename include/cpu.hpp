67ebc6f398a1232df77e633ba011aeb78d962635
Emiliano Firmino <emiliano.firmino@gmail.com>
2016-08-25 15:48:45 -0400

    Implemented some of ADD, ADC and LD instructions
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -36,9 +36,93 @@ public:
 
     void reset();
 
+    // Instruction Template
+    void tLD_r_r(uint8_t&, const uint8_t&);
+    void tADD_A_(const uint8_t&);
+    void tADC_A_(const uint8_t&);
+    void tADD_HL_(const uint8_t&, const uint8_t&);
+    void tADC_HL_(const uint8_t&, const uint8_t&);
+
     // Instruction Set
     void iNOP();
-    void iADD_A_E();
+
+    void iLD_A_A() { tLD_r_r(reg.a, reg.a); }
+    void iLD_A_B() { tLD_r_r(reg.a, reg.b); }
+    void iLD_A_C() { tLD_r_r(reg.a, reg.c); }
+    void iLD_A_D() { tLD_r_r(reg.a, reg.d); }
+    void iLD_A_E() { tLD_r_r(reg.a, reg.e); }
+    void iLD_A_H() { tLD_r_r(reg.a, reg.h); }
+    void iLD_A_L() { tLD_r_r(reg.a, reg.l); }
+
+    void iLD_B_A() { tLD_r_r(reg.b, reg.a); }
+    void iLD_B_B() { tLD_r_r(reg.b, reg.b); }
+    void iLD_B_C() { tLD_r_r(reg.b, reg.c); }
+    void iLD_B_D() { tLD_r_r(reg.b, reg.d); }
+    void iLD_B_E() { tLD_r_r(reg.b, reg.e); }
+    void iLD_B_H() { tLD_r_r(reg.b, reg.h); }
+    void iLD_B_L() { tLD_r_r(reg.b, reg.l); }
+
+    void iLD_C_A() { tLD_r_r(reg.c, reg.a); }
+    void iLD_C_B() { tLD_r_r(reg.c, reg.b); }
+    void iLD_C_C() { tLD_r_r(reg.c, reg.c); }
+    void iLD_C_D() { tLD_r_r(reg.c, reg.d); }
+    void iLD_C_E() { tLD_r_r(reg.c, reg.e); }
+    void iLD_C_H() { tLD_r_r(reg.c, reg.h); }
+    void iLD_C_L() { tLD_r_r(reg.c, reg.l); }
+
+    void iLD_D_A() { tLD_r_r(reg.d, reg.a); }
+    void iLD_D_B() { tLD_r_r(reg.d, reg.b); }
+    void iLD_D_C() { tLD_r_r(reg.d, reg.c); }
+    void iLD_D_D() { tLD_r_r(reg.d, reg.d); }
+    void iLD_D_E() { tLD_r_r(reg.d, reg.e); }
+    void iLD_D_H() { tLD_r_r(reg.d, reg.h); }
+    void iLD_D_L() { tLD_r_r(reg.d, reg.l); }
+
+    void iLD_E_A() { tLD_r_r(reg.e, reg.a); }
+    void iLD_E_B() { tLD_r_r(reg.e, reg.b); }
+    void iLD_E_C() { tLD_r_r(reg.e, reg.c); }
+    void iLD_E_D() { tLD_r_r(reg.e, reg.d); }
+    void iLD_E_E() { tLD_r_r(reg.e, reg.e); }
+    void iLD_E_H() { tLD_r_r(reg.e, reg.h); }
+    void iLD_E_L() { tLD_r_r(reg.e, reg.l); }
+
+    void iLD_H_A() { tLD_r_r(reg.h, reg.a); }
+    void iLD_H_B() { tLD_r_r(reg.h, reg.b); }
+    void iLD_H_C() { tLD_r_r(reg.h, reg.c); }
+    void iLD_H_D() { tLD_r_r(reg.h, reg.d); }
+    void iLD_H_E() { tLD_r_r(reg.h, reg.e); }
+    void iLD_H_H() { tLD_r_r(reg.h, reg.h); }
+    void iLD_H_L() { tLD_r_r(reg.h, reg.l); }
+
+    void iLD_L_A() { tLD_r_r(reg.l, reg.a); }
+    void iLD_L_B() { tLD_r_r(reg.l, reg.b); }
+    void iLD_L_C() { tLD_r_r(reg.l, reg.c); }
+    void iLD_L_D() { tLD_r_r(reg.l, reg.d); }
+    void iLD_L_E() { tLD_r_r(reg.l, reg.e); }
+    void iLD_L_H() { tLD_r_r(reg.l, reg.h); }
+    void iLD_L_L() { tLD_r_r(reg.l, reg.l); }
+
+    void iADD_A_A() { tADD_A_(reg.a); }
+    void iADD_A_B() { tADD_A_(reg.b); }
+    void iADD_A_C() { tADD_A_(reg.c); }
+    void iADD_A_D() { tADD_A_(reg.d); }
+    void iADD_A_E() { tADD_A_(reg.e); }
+    void iADD_A_H() { tADD_A_(reg.h); }
+    void iADD_A_L() { tADD_A_(reg.l); }
+
+    void iADC_A_A() { tADC_A_(reg.a); }
+    void iADC_A_B() { tADC_A_(reg.b); }
+    void iADC_A_C() { tADC_A_(reg.c); }
+    void iADC_A_D() { tADC_A_(reg.d); }
+    void iADC_A_E() { tADC_A_(reg.e); }
+    void iADC_A_H() { tADC_A_(reg.h); }
+    void iADC_A_L() { tADC_A_(reg.l); }
+
+    void iADD_HL_BC() { tADD_HL_(reg.b, reg.c); }
+    void iADD_HL_DE() { tADD_HL_(reg.d, reg.e); }
+    void iADD_HL_HL() { tADD_HL_(reg.h, reg.l); }
+    void iADD_HL_SP();
+
 };
 
 #endif
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -2,16 +2,33 @@
 
 #include <cstring>
 
-inline uint16_t check_zero(int32_t value) {
-    return (value == 0) ? 0x80 : 0x00;
+const uint16_t kFlagZ = 1 << 7;
+const uint16_t kFlagN = 1 << 6;
+const uint16_t kFlagH = 1 << 5;
+const uint16_t kFlagC = 1 << 4;
+
+inline uint16_t check_z(int32_t value) {
+    return (value == 0) ? kFlagZ : 0x00;
 }
 
-inline uint16_t check_negative() {
-    return 0x40;
+inline uint16_t check_n() {
+    return kFlagN;
 }
 
-inline uint16_t check_carry(int32_t value) {
-    return (value > 255 || value < 0) ? 0x10 : 0x00;
+inline uint16_t check_h(uint32_t value) {
+    return (value & 0x10) == 0x10 ? kFlagH : 0x00;
+}
+
+inline uint16_t check_c(int32_t value) {
+    return (value > 0xff || value < 0) ? kFlagC : 0x00;
+}
+
+inline uint16_t check_h2(uint32_t value) {
+    return (value & 0x10) == 0x1000 ? kFlagH : 0x00;
+}
+
+inline uint16_t check_c2(int32_t value) {
+    return (value > 0xffff || value < 0) ? kFlagC : 0x00;
 }
 
 void Z80::reset() {
@@ -22,10 +39,89 @@ void Z80::iNOP() {
     clock += Clock(1);
 }
 
-void Z80::iADD_A_E() {
-    acc = reg.a + reg.e;
-    reg.f = check_zero(acc & 0xff) + check_carry(acc);
+// Template Instruction
+
+/**
+ * LD r1,r2
+ *
+ * Put value r2 into r1
+ */
+void Z80::tLD_r_r(uint8_t& dst, const uint8_t& src) {
+    dst = src;
+    clock += Clock(1);
+}
+
+/**
+ * ADD A,r
+ *
+ * Add r to A
+ * r: A, B, C, D, E, H or L
+ *
+ * Flags Affected:
+ * Z - Set if result is zero
+ * N - Reset
+ * H - Set if carry from bit 3
+ * C - Set if carry from bit 7
+ */
+void Z80::tADD_A_(const uint8_t& r) {
+    acc = reg.a + r;
+    reg.f = check_z(acc & 0xff) + check_h(acc) + check_c(acc);
     reg.a = static_cast<uint8_t>(acc & 0xff);
 
     clock += Clock(1);
+}
+
+/**
+ * ADD HL,rr
+ *
+ * ADD rr to HL
+ * rr: BC, DE, HL, SP
+ *
+ * Flags Affected:
+ * Z - Not affected
+ * N - Reset
+ * H - Set if carry from bit 11
+ * C - Set if carry from bit 15
+ */
+void Z80::tADD_HL_(const uint8_t& rh, const uint8_t& rl) {
+    acc = (reg.h << 8) + reg.l;
+    acc += (rh << 8) + rl;
+
+    reg.f = (reg.f & kFlagZ) + check_h2(acc) + check_c2(acc);
+    reg.h = static_cast<uint8_t>((acc >> 8) & 0xff);
+    reg.l = static_cast<uint8_t>(acc & 0xff);
+
+    clock += Clock(2);
+}
+
+/**
+ * ADC A,r
+ *
+ * ADC (r + carry) to A
+ * r: A, B, C, D, E, H or L
+ *
+ * Flags Affected:
+ * Z - Set if result is zero
+ * N - Reset
+ * H - Set if carry from bit 3
+ * C - Set if carry from bit 7
+ */
+void Z80::tADC_A_(const uint8_t& r) {
+    acc = reg.a + r;
+    acc += reg.f & kFlagC ? 1 : 0;
+
+    reg.f = check_z(acc & 0xff) + check_h(acc) + check_c(acc);
+    reg.a = static_cast<uint8_t>(acc & 0xff);
+
+    clock += Clock(1);
+}
+
+
+void Z80::iADD_HL_SP() {
+    uint8_t s = static_cast<uint8_t>(reg.sp >> 8);
+    uint8_t p = static_cast<uint8_t>(reg.sp & 0xff);
+
+    tADD_HL_(s, p);
+
+    reg.sp = static_cast<uint16_t>((s << 8) + p);
 }
